20c6ca3f8d253c87820aced84197be4180e7303c
abc <abc@example.com>
2023-02-24 11:53:53 +0530

    initial

---
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.javatpoint.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController 
+{
+
+@GetMapping("/get")
+public String hello() 
+{
+return "Hello World";
+}
+
+}
